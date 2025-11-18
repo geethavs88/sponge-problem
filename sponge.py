efb5366e9c06d3691d71f27b01a70d08a59afb4f
geethavs88 <geethavs88@gmail.com>
2025-11-18 15:20:53 -0800

    solved the problem

--- sponge.py
+++ sponge.py
@@ -1,6 +1,40 @@
 def sponge_case(sentence):
     # Write your solution here!
-    pass
+    words = sentence.split()
+    new_sentence = []
+    for word in words:
+        new_word = spong_case_word(word)
+        new_sentence.append(new_word)
+    return " ".join(new_sentence)
+
+def spong_case_word(word):
+    new_word = word[0].lower()
+    for i in range(1, len(word)):
+        if i%2 == 0:
+            new_word += word[i].lower()
+        else:
+            new_word += word[i].upper() 
+    print(new_word)       
+    return new_word           
+
+    
+
+    
+#initialize a variable called newword with lowercase of first letter
+# convert the first letter to lowercase and store it to newword
+#Iterate  through each letter of the word
+# if index is odd change it to uppoer case
+# if index is even change it to lower case.
+
+
+#if there are more than one word in the sentence
+# split the sentence using .split() and store the words to alist
+# loop through each word of the list 
+# call sponge case function for each word
+# Store the value to a new sentence
+
+
+
 
 
 
